bb399f6193a9d4b3b4a20ccb30920a96b934008d
HASUMI Hitoshi <hasumi.hitoshi@tecnicasamba.com>
2016-01-14 22:51:12 +0900

    パネル内UIButtonを整列させた

--- app/views/photo/panel_view.rb
+++ app/views/photo/panel_view.rb
@@ -4,7 +4,6 @@ class PanelView < UIView
   def initWithFrame(frame)
     super(frame)
     self.backgroundColor = UIColor.grayColor
-
     @container = []
     @button = []
     6.times do |i|
@@ -14,20 +13,24 @@ class PanelView < UIView
     ('Ａ'..'Ｌ').each_with_index do |label, index|
       @button[index / 2] << UIButton.rounded_rect.tap do |b|
         b.setTitle(label, forState:UIControlStateNormal)
+        b.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter
         b.on(:touch) { |event| closePhotoView }
       end
     end
+
+    panelWidth = Device.screen.width - Device.screen.height * 1.5
+    buttonWidth = (panelWidth - 10 -10 - 15) / 2
     6.times do |index|
       @container[index] << @button[index][0]
       @container[index] << @button[index][1]
-      self << @container[index]
       Motion::Layout.new do |layout|
         layout.view @container[index]
         layout.subviews button0: @button[index][0], button1: @button[index][1]
         layout.vertical "|[button0]|"
         layout.vertical "|[button1]|"
-        layout.horizontal "|-[button0]-[button1]-|"
+        layout.horizontal "|-10-[button0(#{buttonWidth})]-15-[button1(#{buttonWidth})]-10-|"
       end
+      self << @container[index]
     end
 
     @releaseButton = UIButton.buttonWithType(UIButtonTypeSystem)
@@ -38,7 +41,7 @@ class PanelView < UIView
     Motion::Layout.new do |layout|
       layout.view self
       layout.subviews releaseButton: @releaseButton, ab: @container[0], cd: @container[1], ef: @container[2], gh: @container[3], ij: @container[4], kl: @container[5]
-      layout.vertical "|[ab][cd(==ab)][ef(==ab)][releaseButton(==ab)][gh(==ab)][ij(==ab)][kl(==ab)]|"
+      layout.vertical "|[ab][cd(==ab)][ef(==ab)]-[releaseButton(==ab)]-[gh(==ab)][ij(==ab)][kl(==ab)]|"
       layout.horizontal "|[ab]|"
       layout.horizontal "|[cd]|"
       layout.horizontal "|[ef]|"
