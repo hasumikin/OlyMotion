148e508662e22d827b61ec508c1f9b811b744b3d
HASUMI Hitoshi <hitoshi.hasumi@monstar-lab.com>
2016-01-23 17:50:47 +0900

    パネルの各コンポをまじめにつくり始めた

--- app/views/photo/panel_view.rb
+++ app/views/photo/panel_view.rb
@@ -1,36 +1,114 @@
 class PanelView < UIView
   include DebugConcern
 
+  COMPONENTS = [
+    { # A 0 / 0
+      uiType: :button,
+      title: '☓',
+      action: :closePhotoView,
+      outlet: :closePhotoViewButton
+    },
+    { # B 0 / 1
+      uiType: :label,
+      text: 'alert',
+      outlet: :alertLabel
+    },
+    { # C 1 / 0
+      uiType: :button,
+      title: 'WB',
+      action: :toggleWhiteBalance,
+      outlet: :toggleWhiteBalanceButton
+    },
+    { # D 1 / 1
+      uiType: :button,
+      title: 'P',
+      action: :toggleAEMode,
+      outlet: :toggleAEModeButton
+    },
+    { # E 2 / 0
+      uiType: :label,
+      text: '10000',
+      outlet: :shutterSpeedLabel
+    },
+    { # F 2 / 1
+      uiType: :label,
+      text: 'ISO',
+      outlet: :isoLabel
+    },
+    { # G 3 / 0
+      uiType: :button,
+      title: 'AF',
+      action: :toggleFocusMode,
+      outlet: :toggleFocusModeButton
+    },
+    { # H 3 / 1
+      uiType: :button,
+      title: 'AEL',
+      action: :toggleAELock,
+      outlet: :toggleAELockButton
+    },
+    { # I
+      uiType: :label,
+      text: '25',
+      outlet: :focusLengthLabel
+    },
+    { # J
+      uiType: :label,
+      text: '300',
+      outlet: :focusLengthLabel2
+    },
+    { # K
+      uiType: :label,
+      text: '5.6',
+      outlet: :apertureLabel
+    },
+    { # L
+      uiType: :label,
+      text: '±0',
+      outlet: :exposureCorrectLabel
+    }
+  ]
+
   def initWithFrame(frame)
     super(frame)
     self.backgroundColor = UIColor.grayColor
-    @container = []
-    @button = []
+    @containers = []
+    @components = []
     6.times do |i|
-      @container << UIView.new
-      @button[i] = []
+      @containers << UIView.new
+      @components[i] = []
     end
-    ('Ａ'..'Ｌ').each_with_index do |label, index|
-      @button[index / 2] << UIButton.rounded_rect.tap do |b|
-        b.setTitle(label, forState:UIControlStateNormal)
-        b.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter
-        b.on(:touch) { |event| closePhotoView }
+    COMPONENTS.each_with_index do |component, index|
+      case component[:uiType]
+      when :button
+        @components[index / 2] << UIButton.rounded_rect.tap do |b|
+          b.setTitle(component[:title], forState:UIControlStateNormal)
+          b.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter
+          b.on(:touch) { |event| send(component[:action]) }
+        end
+      when :label
+        @components[index / 2] << UILabel.new.tap do |l|
+          l.font = UIFont.systemFontOfSize(12)
+          l.text = component[:text]
+          # l.alignment = UITextAlignmentCenter
+          l.textColor = UIColor.whiteColor
+        end
       end
     end
 
     panelWidth = Device.screen.width - Device.screen.height * 1.5
-    buttonWidth = (panelWidth - 10 -10 - 15) / 2
+    componentWidth = (panelWidth - 5 - 5 - 5) / 2
     6.times do |index|
-      @container[index] << @button[index][0]
-      @container[index] << @button[index][1]
+      @containers[index] << @components[index][0]
+      @containers[index] << @components[index][1]
       Motion::Layout.new do |layout|
-        layout.view @container[index]
-        layout.subviews button0: @button[index][0], button1: @button[index][1]
-        layout.vertical "|[button0]|"
-        layout.vertical "|[button1]|"
-        layout.horizontal "|-10-[button0(#{buttonWidth})]-15-[button1(#{buttonWidth})]-10-|"
+        layout.view @containers[index]
+        layout.subviews component0: @components[index][0], component1: @components[index][1]
+        layout.vertical "|[component0]|"
+        layout.vertical "|[component1]|"
+        layout.horizontal "|-5-[component0(#{componentWidth})]-5-[component1(#{componentWidth})]-5-|"
       end
-      self << @container[index]
+      self << @containers[index]
     end
 
     @releaseButton = UIButton.buttonWithType(UIButtonTypeSystem)
@@ -40,7 +118,7 @@ class PanelView < UIView
 
     Motion::Layout.new do |layout|
       layout.view self
-      layout.subviews releaseButton: @releaseButton, ab: @container[0], cd: @container[1], ef: @container[2], gh: @container[3], ij: @container[4], kl: @container[5]
+      layout.subviews releaseButton: @releaseButton, ab: @containers[0], cd: @containers[1], ef: @containers[2], gh: @containers[3], ij: @containers[4], kl: @containers[5]
       layout.vertical "|[ab][cd(==ab)][ef(==ab)]-[releaseButton(==ab)]-[gh(==ab)][ij(==ab)][kl(==ab)]|"
       layout.horizontal "|[ab]|"
       layout.horizontal "|[cd]|"
